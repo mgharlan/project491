69df8daaecbce967901466817f069adf6c80972d
Mason Harlan <mgharlan@uci.edu>
2021-11-14 10:26:48 -0800

    continued adding stuff

--- main.py
+++ main.py
@@ -25,29 +25,34 @@ class Company(Enum):
 	LOCKSMITH = 4
 	AUCTION = 5
 
-class MiningPool:
+class TransactionPool:
 	transaction_pool = []
 	is_empty = True
 
-	def addToMiningPool(self, transaction : str) -> None:
-		is_empty = False
+	def add(self, transaction : str) -> None:
+		self.is_empty = False
 		self.transaction_pool.append(transaction)
+		# print('add: ', self.transaction_pool)
 
-	def popTransactionFromMiningPool(self) -> str:
+	def pop(self) -> str:
 		if(not self.is_empty):
 			value = self.transaction_pool.pop(0)
 			if(len(self.transaction_pool) == 0):
 				self.is_empty = True
+
+			# print('pop: ', self.transaction_pool)
 			return value
 		else:
+			# print('pop: ', self.transaction_pool)
 			return ''
 
 class Node(ABC):	
-	id = 0
-	name = ''
-	def __init__(self, id, name):
+	transactionPool = TransactionPool()
+
+	def __init__(self, id = 0, name = ''):
 		self.id = id
 		self.name = name
+		print(f"{type(self).__name__} node created: {self.name}({self.id})")
 
 	@property
 	@abstractmethod
@@ -63,40 +68,46 @@ class Recovery(Node):
 	miner = True
 
 	def performOperation(self, VIN):
-		print("VIN: " + VIN)
+		Node.transactionPool.add(f"{Operation.RECOVER.value}|{VIN}|{self.id}")
+		print(f"{self.name}({self.id}) recovered VIN: {VIN}")
 
 class Auction(Node):
 	miner = True
 
 	def performOperation(self, VIN):
-		pass
+		Node.transactionPool.add(f"{Operation.AUCTION.value}|{VIN}|{self.id}")
+		print(f"{self.name}({self.id}) auctioned VIN: {VIN}")
 
 
 class Locksmith(Node):
 	miner = False
 
-	def performOperation(self, VIN):
-		pass
+	def performOperation(self, VIN):		
+		Node.transactionPool.add(f"{Operation.CUT_KEYS.value}|{VIN}|{self.id}")
+		print(f"{self.name}({self.id}) cut keys for VIN: {VIN}")
 
 class Transport(Node):
 	miner = False
 
 	def performOperation(self, VIN, origin, destination):
-		pass
+		Node.transactionPool.add(f"{Operation.TRANSPORT.value}|{VIN}|{self.id}|{origin}|{destination}")
+		print(f"{self.name}({self.id}) transported VIN: {VIN} from {Main.companies[int(origin)].name}({origin}) to {Main.companies[int(destination)].name}({destination})")
 
 class Mechanic(Node):
 	miner = False
 
 	def performOperation(self, VIN):
-		pass
+		Node.transactionPool.add(f"{Operation.REPAIR.value}|{VIN}|{self.id}")
+		print(f"{self.name}({self.id}) repaired VIN: {VIN}")
 
 class Main:
 	VIN_length = 11
 	companies = dict()
-	company_ledger = [[100, 'RecoveryInc', Company.RECOVERY],
+	miners = []
+	company_ledger = [[100, 'RecoveryInc', Company.RECOVERY, 2],
 	 [200, 'TransportCo', Company.TRANSPORT], [201, 'TransportInc', Company.TRANSPORT],
 	  [300, 'MechanicLtd', Company.MECHANIC], [400, 'LocksmithCo', Company.LOCKSMITH],
-	   [500, 'AuctionCo', Company.AUCTION], [501, 'AuctionLLC', Company.AUCTION]]
+	   [500, 'AuctionCo', Company.AUCTION, 2], [501, 'AuctionLLC', Company.AUCTION, 3]]
 
 	def run(self):
 		self.setupNodes();	
@@ -105,22 +116,42 @@ class Main:
 	def setupNodes(self):
 		for company in self.company_ledger:
 			if(company[2] == Company.RECOVERY):	
-				self.companies[company[0]] = Recovery(company[0], company[1])
+				Main.companies[company[0]] = Recovery(company[0], company[1])
+				Main.miners.extend(company[3] * [company[0]])
 			elif(company[2] == Company.TRANSPORT):	
-				self.companies[company[0]] = Transport(company[0], company[1])
+				Main.companies[company[0]] = Transport(company[0], company[1])
 			elif(company[2] == Company.LOCKSMITH):	
-				self.companies[company[0]] = Locksmith(company[0], company[1])
+				Main.companies[company[0]] = Locksmith(company[0], company[1])
 			elif(company[2] == Company.MECHANIC):	
-				self.companies[company[0]] = Mechanic(company[0], company[1])
+				Main.companies[company[0]] = Mechanic(company[0], company[1])
 			elif(company[2] == Company.AUCTION):	
-				self.companies[company[0]] = Auction(company[0], company[1])
+				Main.companies[company[0]] = Auction(company[0], company[1])
+				Main.miners.extend(company[3] * [company[0]])
 
 	def runSimulation(self):
+		firstVIN = self.generateVIN()
 		#RecoveryCo recovers a car and logs the operation
-		self.companies[100].performOperation(self.generateVIN())
+		Main.companies[100].performOperation(firstVIN)
+		#TransportCo takes the car from RecoveryCo and delivers it to MechanicLtd
+		Main.companies[200].performOperation(firstVIN, 100, 300)
+		#MechanicLtd repairs the car
+		Main.companies[300].performOperation(firstVIN)
+		#TransportInc takes the car from MechanicLtd and delivers it to AuctionCo
+		Main.companies[201].performOperation(firstVIN, 300, 500)
+		#AuctionCo auctions the car
+		Main.companies[500].performOperation(firstVIN)
+
+		self.runMining()
+
+	def runMining(self):
+		random.shuffle(Main.miners)
 
 	def generateVIN(self):
 		return ''.join(random.choices(string.ascii_uppercase + string.digits, k = self.VIN_length))    
+
+	def trace(self, VIN):
+		# print(Operation(int(Node.transactionPool.pop().split('|')[0])) == Operation.RECOVER)
+		pass
 
 if __name__ == "__main__":	
 	Main().run()
